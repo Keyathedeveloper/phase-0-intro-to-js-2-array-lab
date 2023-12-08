7e7fa838a78bb9780c065c77aa27714c82ab0d3c
TaKeya McFadden <takeyamcfadden@gmail.com>
2023-12-08 08:38:08 -0500

    Final commit after 7hrs of frustration. Lol

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop("");
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("");
+}
+
+function appendCat(name) {
+    const newCatsArray = cats.concat("Broom");
+    return newCatsArray;
+}
+
+function prependCat(name) {
+    const newCatsArray = ["Arnold", ...cats];
+    return newCatsArray;
+}
+
+function removeLastCat() {
+    const newCatsArray = cats.slice(0, -1);
+    return newCatsArray;
+}
+
+function removeFirstCat() {
+    const newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
+
+module.exports = {
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveLastCat,
+    destructivelyRemoveFirstCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat
+};
